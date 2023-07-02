64c4af69587707825d0c51d487aea28c5395924c
Kuba <339630@uwr.edu.pl>
2023-07-02 18:53:46 +0200

    Moved tests out of main file

---
+++ tests.cpp
@@ -0,0 +1,36 @@
+// {
+    //     cout << "<<< 1 >>>" << endl;
+    //     Parser parser;
+    //     string s = "set x to 3 1 +";
+    //     parser.parse_set(s);
+    // }
+    // {
+    //     cout << "<<< 2 >>>" << endl;
+    //     Parser parser;
+    //     string s = "set x to 9";
+    //     parser.parse_set(s);
+    // }
+    // {
+    //     cout << "<<< 3 >>>" << endl;
+    //     Parser parser;
+    //     string s = "set x to ";
+    //     parser.parse_set(s);
+    // }
+    // {
+    //     cout << "<<< 4 >>>" << endl;
+    //     Parser parser;
+    //     string s = "set x  to ";
+    //     parser.parse_set(s);
+    // }
+    // {
+    //     cout << "<<< 5 >>>" << endl;
+    //     Parser parser;
+    //     string s = "set     x to ";
+    //     parser.parse_set(s);
+    // }
+    // {
+    //     cout << "<<< 6 >>>" << endl;
+    //     Parser parser;
+    //     string s = "set     x banan ";
+    //     parser.parse_set(s);
+    // }
